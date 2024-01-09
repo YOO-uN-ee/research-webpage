e1872c7fdee64f9630aa8b8399d560b1e0f7434a
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-01-08 22:05:52 -0600

    configured for other data

--- client/src/pages/UserTypes/Admin.js
+++ client/src/pages/UserTypes/Admin.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { json2csv } from 'json-2-csv';
-import { mkConfig, generateCsv, download } from "export-to-csv";
-import csvDownload from 'json-to-csv-export'
 
 const Detailed = () => {
-
-  const [product, setProduct] = useState([]);
+  const [control, setControl] = useState([]);
+  const [one, setOne] = useState([]);
+  const [two, setTwo] = useState([]);
+  const [three, setThree] = useState([]);
+  // const [all, setAll] = useState([]);
 
 
   const getAllProducts = async()=> {
     try {
       const { data } = await axios.get("https://research-backend-3mwd.onrender.com/api/auth/getdata/control");
-      setProduct(data.userdata);
+      setControl(data.userdata);
+
+      const { dataone } = await axios.get("https://research-backend-3mwd.onrender.com/api/auth/getdata/treatment1");
+      setOne(dataone.userdata);
+
+      const { datatwo } = await axios.get("https://research-backend-3mwd.onrender.com/api/auth/getdata/treatment2");
+      setTwo(datatwo.userdata);
+
+      const { datathree } = await axios.get("https://research-backend-3mwd.onrender.com/api/auth/getdata/treatment3");
+      setThree(datathree.userdata);
+
     } catch (error) {
       console.log(error);
     }
@@ -23,26 +34,34 @@ const Detailed = () => {
 
   }, []);
 
-  const downloadCSV = () => {
-    const control_data = json2csv(product)
-    document.write(control_data);
+  const downloadCSV = (input_data, filename) => {
+    const converted_data = json2csv(input_data)
+    document.write(converted_data);
 
     var hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:text/csv;charset=utf-8,\uFEFF' + encodeURI(control_data);
+    hiddenElement.href = 'data:text/csv;charset=utf-8,\uFEFF' + encodeURI(converted_data);
     hiddenElement.target = '_blank';
 
-    hiddenElement.download = 'Dummy.csv';
+    hiddenElement.download = `${filename}.csv`;
     hiddenElement.click();
   }
 
   return (
     <>
-      {product?.map((p) => (
-        <div>{p.ip}</div>
-      ))}
-      {/* {control_data} */}
-      <button onClick={() => downloadCSV()}>
-        Download
+      <button onClick={() => downloadCSV(control, 'control')}>
+        Download Control Group Data
+      </button>
+
+      <button onClick={() => downloadCSV(one, 'one')}>
+        Download Treatment1 Group Data
+      </button>
+
+      <button onClick={() => downloadCSV(two, 'two')}>
+        Download Treatment2 Group Data
+      </button>
+
+      <button onClick={() => downloadCSV(three, 'three')}>
+        Download Treatment3 Group Data
       </button>
     </>
   )
