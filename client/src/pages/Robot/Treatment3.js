1f92c81e54936cd01fcf9d0a9784ca1588d04832
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-01-08 22:46:21 -0600

    final push

--- client/src/pages/Robot/Treatment3.js
+++ client/src/pages/Robot/Treatment3.js
@@ -28,7 +28,10 @@ const Intro1 = () => {
   return (
     <Layout>
       <div class='main-text general-body'>
-        이 실험에 참여해 주셔서 감사합니다!
+        이 실험에 참여해 주셔서 감사합니다! <br></br><br></br>
+
+        이 실험에서 당신은 동네 슈퍼마켓에서 쇼핑하는 상황을 온라인에서 재현하게 될 것입니다.<br></br>
+        실제로 쇼핑하는 것처럼 질문에 답하여 참여해 주십시오.
       </div>
 
       <div class='right-button-parent'><Link to='./2'><img src='/media/images/forward.svg' class='right-button' alt='forward'/></Link></div>
