fed432bb13daa4b1d8e01792ca8c0123a7cad701
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-01-10 18:55:55 -0600

    check if all option and times are working for all scenarios

--- client/src/pages/Robot/Robot5.js
+++ client/src/pages/Robot/Robot5.js
@@ -22,9 +22,9 @@ const Robot5 = () => {
   localStorage.setItem('arrive_used', -1);
   localStorage.setItem('recom_used', -1);
 
-  localStorage.setItem('treatment_sequence', JSON.stringify([]))
+  localStorage.setItem('treatment_option', JSON.stringify([]))
   localStorage.setItem('treatment_time', JSON.stringify([]))
-  localStorage.setItem('add_item', JSON.stringify([]))
+  localStorage.setItem('sub_action', JSON.stringify([]))
 
   if (!TreatmentSet){
     if (extension === 'FFSRWM' || extension === 'ZOX019'){
