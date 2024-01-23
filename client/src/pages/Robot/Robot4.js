e4b67c33c0a5d5282b8696c215d8913ad6500c2a
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-01-23 14:20:34 -0600

    added additional sentence on page 4 on treat

--- client/src/pages/Robot/Robot4.js
+++ client/src/pages/Robot/Robot4.js
@@ -38,6 +38,8 @@ const Robot = () => {
             id="inputName"
             placeholder="홍길동"
           />
+          <br/>
+          이름이나 닉네임을 작성하여도 무방하고, 이름을 작성하지 않아도 다음으로 넘어갈 수 있습니다.
          </div>
       </div>
 
