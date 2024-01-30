9e2db3004ad3db542917d6bc28505cfde506dd5f
Jiyoon Pyo <jiyoonp0228@gmail.com>
2024-01-30 13:37:19 -0600

    Update AutoRedirect.js

--- client/src/pages/UserTypes/AutoRedirect.js
+++ client/src/pages/UserTypes/AutoRedirect.js
@@ -1,21 +1,19 @@
 import React, { useEffect } from 'react'
 
 const AutoRedirect = () => {
-  useEffect(() => {
-    localStorage.clear();
-    
-    var a = window.location.href; 
-    var b = a.substring(a.indexOf("?UID=")+5);
-
-    localStorage.setItem('uid', b)
-  }, []);
-
     const number_mapping = {
         0: 'D1UQDV',
         1: 'FFSRWM',
         2: 'L53NNR',
     }
     useEffect(() => {
+      localStorage.clear();
+    
+      var a = window.location.href; 
+      var b = a.substring(a.indexOf("?UID=")+5);
+  
+      localStorage.setItem('uid', b)
+      
        const random_assignment = number_mapping[Math.floor(Math.random() * 3)];
         window.location.replace(`./${random_assignment}/welcome`);
       }, []);
